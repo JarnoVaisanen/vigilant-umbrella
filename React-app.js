7a7e67a9fe27d09a720dff5e4c744231670e5d01
Jarno Väisänen <145106444+JarnoVaisanen@users.noreply.github.com>
2026-01-13 13:26:24 +0200

    Update and rename React-app to React-app.js

---
+++ React-app.js
@@ -0,0 +1,31 @@
+import react, from { usestate } from 'react'
+
+function ToDolist(){
+
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = usestate("");
+
+    function handleInputChange(event){
+
+    }
+
+    function addtask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+
+    function moveTaskDown(index){
+        
+    }
+
+    return (<></>);
+}
+export default ToDoList
+
